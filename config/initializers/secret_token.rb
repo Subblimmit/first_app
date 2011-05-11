f3c24ac5dcd114b0d732c8ee55d7adf6ccebed14
sublimit <subrange636@gmail.com>
2011-05-11 23:42:17 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'a639916ee75d104d04734cc803dafb3a45ecfaad6612626674f219d6104a59a35b3b9143be442ee2cc6d75fd06cd4da38a4b8856758589d05a1acd7eb8270d5b'
